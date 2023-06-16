22e5f5219670980ba93125ec3785accb7a3bbe36
Alicja <alanotowska@yahoo.co.uk>
2023-06-16 12:23:28 +0200

    installs dependencies, creates main files and src folder

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client';
+
+//Import statement to indicate that I need to bundle `./index.scss`
+import "./index.scss";
+
+const myFlixApplication = () => {
+    return (
+        <div className="my-flix">
+            <div>Good morning</div>
+        </div>
+    );
+};
+
+//Finds the root of my app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+//Tells React to render my app in the root DOM element
+root.render(<MyFlixApplication />);
