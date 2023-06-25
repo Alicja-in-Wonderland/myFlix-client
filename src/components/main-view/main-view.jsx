69c509b05b90b6e8cb2266529506212e2c29ae4b
Alicja <alanotowska@yahoo.co.uk>
2023-06-25 13:25:25 +0200

    Create main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,9 @@
+export const MainView = () => {
+    return (
+        <div>
+            <div>The Dark Knight</div>
+            <div>Silence of the Lambs</div>
+            <div>Se7en</div>
+        </div>
+    );
+};
