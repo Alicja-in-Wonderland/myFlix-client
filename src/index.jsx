a532c86d85ebba9aeca2f68764fb3f881676afe3
Alicja <alanotowska@yahoo.co.uk>
2023-06-25 13:35:50 +0200

    imports MainView into index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
 
 //Import statement to indicate that I need to bundle `./index.scss`
 import "./index.scss";
 
-const MyFlixApplication = () => {
-    return (
-        <div className="my-flix">
-            <div>Good morning</div>
-        </div>
-    );
+const App = () => {
+    return <MainView />;
 };
 
 //Finds the root of my app
@@ -16,4 +13,4 @@ const container = document.querySelector("#root");
 const root = createRoot(container);
 
 //Tells React to render my app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<App />);
