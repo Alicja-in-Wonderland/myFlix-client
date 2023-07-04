9289195a4c37f822d8c8f94839f153f80ae55d76
Alicja <alanotowska@yahoo.co.uk>
2023-07-04 19:56:38 +0200

    uses useState in MainView to store a list of movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 export const MainView = () => {
-    return (
-        <div>
-            <div>The Dark Knight</div>
-            <div>Silence of the Lambs</div>
-            <div>Se7en</div>
-        </div>
-    );
+    const [movies, setMovies] = useState([
+        { id: 1, title: "The Dark Knight" },
+        { id: 2, title: "Silence of the Lambs" },
+        { id: 3, title: "Se7en" }
+    ]);
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    } else {
+        return (
+            <div>
+                {movies.map((movie) => {
+                    return <div key={movie.id}>{movie.title}</div>;
+                })}
+            </div>
+        );
+    }
 };
