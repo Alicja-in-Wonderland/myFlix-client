90fc66329ed66bbbde50dc7b8e95d0686eab191f
Alicja <alanotowska@yahoo.co.uk>
2023-07-06 18:00:48 +0200

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29090,7 +29090,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51534" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57201" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
