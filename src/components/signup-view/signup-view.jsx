7f4400e05d8628532bb3234acc76f091c98b6d0f
Alicja <alanotowska@yahoo.co.uk>
2023-07-31 12:57:05 +0200

    Fixes the React import

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,5 +1,6 @@
 
-import { React, useState } from "react";
+import React from "react"
+import { useState } from "react";
 
 export const SignupView = () => {
     const [username, setUsername] = useState("");
